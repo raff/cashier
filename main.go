39c84fd6d7ff09339dc6f11b919aa69dbd8d0223
Raffaele Sena <raff367@gmail.com>
2018-12-13 16:39:47 -0800

    Plugged in storage service. Now you can uplaod a file, but not download it :(

--- main.go
+++ main.go
@@ -1,48 +1,127 @@
 package main
 
 import (
+	"flag"
+	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/raff/cashier/storage"
 )
 
 type Cashier struct {
-	cache map[string]interface{}
+	sdb *storage.StorageDB
 }
 
 func (cc *Cashier) getEntry(c echo.Context) error {
 	id := c.Param("id")
-	if v, ok := cc.cache[id]; ok {
-		return c.JSON(http.StatusOK, map[string]interface{}{id: v})
+	info, err := cc.sdb.Stat(id)
+	if err == storage.ErrNotFound {
+		return c.String(http.StatusNotFound, "NOT FOUND")
+	}
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	return c.String(http.StatusNotFound, "NOT FOUND")
+	return c.JSON(http.StatusOK, info)
 }
 
 func (cc *Cashier) putEntry(c echo.Context) error {
 	id := c.Param("id")
-	if _, ok := cc.cache[id]; ok {
-		return c.JSON(http.StatusConflict, "ALREADY EXISTS")
+
+	var reader io.Reader
+	var size int64
+
+	finfo, err := c.FormFile("file")
+	if err == nil {
+		ffile, ferr := finfo.Open()
+		if ferr != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		defer ffile.Close()
+
+		err = cc.sdb.CreateFile(id, finfo.Filename, finfo.Header.Get("Content-Type"), finfo.Size, nil)
+		reader = ffile
+		size = finfo.Size
+	} else if err == http.ErrNotMultipart {
+		// not a form, we just read the body
+		err = cc.sdb.CreateFile(id, id, c.Request().Header.Get("Content-Type"), c.Request().ContentLength, nil)
+		reader = c.Request().Body
+		size = c.Request().ContentLength
 	}
 
-	cc.cache[id] = id
+	if err == storage.ErrExists {
+		return c.JSON(http.StatusConflict, "ALREADY EXISTS")
+	}
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	var buf = make([]byte, storage.BlockSize)
+	var nread int64
+
+	for pos := int64(0); pos != storage.FileComplete; {
+		var n int
+
+		n, err = reader.Read(buf)
+		if err == io.EOF {
+			if n == 0 {
+				break
+			}
+		} else if err != nil {
+			break
+		}
+
+		npos, err := cc.sdb.WriteAt(id, pos, buf[:n])
+		if err != nil {
+			break
+		}
+
+		nread += int64(n)
+		pos = npos
+	}
+
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	if nread != size {
+		log.Println("expected", size, "read", nread)
+	}
+
 	return c.String(http.StatusCreated, "CREATED")
 }
 
 func (cc *Cashier) deleteEntry(c echo.Context) error {
 	id := c.Param("id")
-	delete(cc.cache, id)
+	if err := cc.sdb.DeleteFile(id); err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
 	return c.String(http.StatusNoContent, "DELETED")
 }
 
-var (
-	Cache = &Cashier{cache: map[string]interface{}{}}
-)
-
 func main() {
+	path := flag.String("path", "storage.data", "path to data folder")
+	ttl := flag.Duration("ttl", 10*time.Minute, "time to live")
+	//gc := flag.Bool("gc", false, "run value-log gc")
+
+	flag.Parse()
+
+	sdb, err := storage.Open(*path, *ttl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sdb.Close()
+
 	// Echo instance
 	e := echo.New()
+	cashier := &Cashier{sdb: sdb}
 
 	// Middleware
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
@@ -58,9 +137,9 @@ func main() {
 		return c.JSON(http.StatusOK, e.Routes())
 	})
 
-	e.GET("/x/:id", Cache.getEntry)
-	e.POST("/x/:id", Cache.putEntry)
-	e.DELETE("/x/:id", Cache.deleteEntry)
+	e.GET("/x/:id", cashier.getEntry)
+	e.POST("/x/:id", cashier.putEntry)
+	e.DELETE("/x/:id", cashier.deleteEntry)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1999"))
